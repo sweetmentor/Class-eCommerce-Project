80b40037b0bef737688273dd2b813e7531646a19
Stephen Afolabi <stephenafolabi@gmail.com>
2018-07-02 09:58:03 +0000

    working app with stripe

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -27,5 +27,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-SYSTEM_EMAIL = 'yourname@example.com'
+SYSTEM_EMAIL = 'admin@example.com'
 
